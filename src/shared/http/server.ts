bbf4c8ea6fdf448c6917d3a88ce2095a21a3992e
Kalu <kalu@pop-os.localdomain>
2022-09-02 20:00:57 -0300

    config .gitignore and change local port

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -9,6 +9,6 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(3333, () => {
-    console.log('Server started on port 3333!');
+app.listen(3000, () => {
+    console.log('Server started on port 3000!');
 });
